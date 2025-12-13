47fc5c88f7b6beab34a0614c9748449c64974f7b
elena marquez <elena-marquez@csu.fullerton.edu>
2025-12-13 02:29:27 -0800

    Fix heart and colors in mobile view

--- src/Envelope.jsx
+++ src/Envelope.jsx
@@ -2,8 +2,12 @@ import './Envelope.css'
 import myHeart from "./assets/heart-cartoon-style.png"
 
 function Envelope({ onClick }) {
+  const handleEnvelopeClick = () => {
+    
+  }
+
   return (
-    <div className="envelope-container">
+    <div className="envelope-container" onClick={handleEnvelopeClick}>
       <div className="envelope">
         <div className="envelope-bottom"></div>
         <div className="flap"></div>
